30f1322f496c53090bcf39478ee91e42bf545aa5
shanthani <46441058+shanthani@users.noreply.github.com>
2019-01-25 22:52:44 +0530

    Create repeated_one.py

---
+++ repeated_one.py
@@ -0,0 +1,13 @@
+#shan
+r=int(input())
+n=input()
+d={}
+a=[]
+for i in n.split():
+    u=n.split()
+    c=u.count(i)
+    d.update({i:c})
+    a.append(c)
+for x,y in d.items():
+    if y==1:
+        print(x)
