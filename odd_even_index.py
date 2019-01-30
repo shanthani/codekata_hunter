e2583c3ef08b4f2720d278de378e8c91048bffda
shanthani <46441058+shanthani@users.noreply.github.com>
2019-01-30 15:33:05 +0530

    Update odd_even_index.py

--- odd_even_index.py
+++ odd_even_index.py
@@ -2,8 +2,15 @@
 n=int(input())
 m=list(map(int,input().split()))
 c=0
+a=[]
 for i in range(n):
         if i%2==0 and m[i]%2==1:
-            a=m[i]
+            a.append(m[i])
         elif i%2==1 and m[i]%2==0:          
-            b=m[i]
+            a.append(m[i])
+for j in a:
+    if c==0:
+        print(j,end="")
+        c+=1
+    else:
+        print("",j,end="")
